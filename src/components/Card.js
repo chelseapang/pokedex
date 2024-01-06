11ced824adcf2a7c675ade469c60a416eb82daf5
Chelsea Pang <chelsea.pang96@gmail.com>
2024-01-05 20:58:10 -0800

    updated colors for background, cards, page numbers

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Card = ({ name, imageUrl }) => {
     return (
-        <div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
+        <div className='tc bg-washed-green dib br3 pa3 ma2 grow bw2 shadow-5'>
             <img alt='pokemon' src={imageUrl} />
             <div>
                 <h2>{name}</h2>
