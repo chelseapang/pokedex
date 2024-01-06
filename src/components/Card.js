84a31e2a75b59abafe008c29b9ece91b9be81169
Chelsea Pang <chelsea.pang96@gmail.com>
2024-01-05 21:26:58 -0800

    updated styling - font, rounded borders, card size

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import './Card.css';
 
 const Card = ({ name, imageUrl }) => {
     return (
-        <div className='tc bg-washed-green dib br3 pa3 ma2 grow bw2 shadow-5'>
+        <div className='card tc bg-washed-green dib br3 pa3 ma2 grow bw2 shadow-5'>
             <img alt='pokemon' src={imageUrl} />
             <div>
                 <h2>{name}</h2>
